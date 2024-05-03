2291c75c3158a62c2812bb70a1489890f792aab3
hokyun <wjdghrbs2468@naver.com>
2024-05-03 14:48:17 +0900

    feat: add AWS status check code

--- src/main/java/com/example/albaya/MainController.java
+++ src/main/java/com/example/albaya/MainController.java
@@ -1,11 +1,18 @@
 package com.example.albaya;
 
+import org.springframework.http.HttpStatus;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
+    @GetMapping("/healthcheck")
+    public ResponseEntity healthCheck(){
+        return new ResponseEntity(HttpStatus.OK);
+    }
     @GetMapping("/")
     public String home(){
         System.out.println("home 요청 들어옴");
